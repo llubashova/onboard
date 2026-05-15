fb6de29446b3a270ae68d4436ee713e258c9814d
llubashova <llubashova@vk.com>
2026-05-15 20:21:10 +0300

    fix: sync progress (ao_p_* keys) to cloud so HR can see all employees' XP

--- db.js
+++ db.js
@@ -7,6 +7,28 @@ const DB = (() => {
 
   let _syncing = false;
 
+  // Собрать весь прогресс из localStorage в объект { "ao_p_<id>_<key>": "1" }
+  function collectProgress() {
+    const progress = {};
+    for (let i = 0; i < localStorage.length; i++) {
+      const k = localStorage.key(i);
+      if (k && k.startsWith('ao_p_')) {
+        progress[k] = localStorage.getItem(k);
+      }
+    }
+    return progress;
+  }
+
+  // Восстановить прогресс из облака в localStorage
+  function restoreProgress(progress) {
+    if (!progress || typeof progress !== 'object') return;
+    Object.keys(progress).forEach(k => {
+      if (k.startsWith('ao_p_')) {
+        localStorage.setItem(k, progress[k]);
+      }
+    });
+  }
+
   const cloudFetch = fetch(URL, {
     headers: { 'X-Master-Key': API_KEY, 'X-Bin-Meta': 'false' }
   })
@@ -15,6 +37,7 @@ const DB = (() => {
     if (cloud && cloud.users && Object.keys(cloud.users).length > 0) {
       localStorage.setItem('ao_users',   JSON.stringify(cloud.users));
       localStorage.setItem('ao_invites', JSON.stringify(cloud.invites || '{}'));
+      restoreProgress(cloud.progress);
     }
   })
   .catch(() => {});
@@ -32,8 +55,9 @@ const DB = (() => {
       method:  'PUT',
       headers: { 'Content-Type': 'application/json', 'X-Master-Key': API_KEY },
       body: JSON.stringify({
-        users:   JSON.parse(localStorage.getItem('ao_users')   || '{}'),
-        invites: JSON.parse(localStorage.getItem('ao_invites') || '{}')
+        users:    JSON.parse(localStorage.getItem('ao_users')   || '{}'),
+        invites:  JSON.parse(localStorage.getItem('ao_invites') || '{}'),
+        progress: collectProgress()
       })
     })
     .catch(() => {})
